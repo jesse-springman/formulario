ef20daf8d4843c5a4d3ad3bef03e1ae6645feb7e
Jesse Springman <jessebarbosa45@gmail.com>
2023-09-13 18:14:39 -0300

    formulario versao1

---
+++ src/js/formulario.js
@@ -0,0 +1,29 @@
+
+const enviar = document.getElementById('enviar');
+
+
+enviar.addEventListener('click', ()=>{
+    const campos = document.querySelectorAll('.input');
+
+
+   const mensagemErro = document.querySelectorAll('.nao-preenchido')
+
+
+   campos.forEach(function(item, indice){
+
+        if(item.value === ""){
+        
+       item.classList.add('borda-red');
+       mensagemErro[indice].classList.add('mostrar');
+       }
+   
+
+       else{
+        item.classList.add('borda-green');
+        mensagemErro[indice].classList.remove('mostrar')
+         }
+       
+      })
+    })
+
+
